805e602a5c70e572593a83a623b15c7502947eb8
jschang19 <wwwsean13579@gmail.com>
2025-09-24 02:34:17 +0800

    chore: replaced link

--- app/root.tsx
+++ app/root.tsx
@@ -14,16 +14,8 @@ import AppFooter from './components/Footer';
 import { GoogleAnalytics } from './components/GoogleAnalytics';
 
 export const links: Route.LinksFunction = () => [
-  { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
-  {
-    rel: 'preconnect',
-    href: 'https://fonts.gstatic.com',
-    crossOrigin: 'anonymous',
-  },
-  {
-    rel: 'stylesheet',
-    href: 'https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap',
-  },
+  { rel: 'preconnect', href: 'https://www.googletagmanager.com' },
+  { rel: 'dns-prefetch', href: 'https://www.googletagmanager.com' },
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
